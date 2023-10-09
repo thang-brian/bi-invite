672a09c0ea98359d51ddcc13b8e5d84658992482
Thân Trọng Thắng <thangthantrong@gmail.com>
2023-10-09 23:50:52 +0700

    first commit

---
+++ reveal.js
@@ -0,0 +1,23 @@
+var AnimationModule = (function () {
+  function initialize() {
+    reveal();
+  }
+  function reveal() {
+    let reveals = document.querySelectorAll(".reveal");
+    for (let i = 0; i < reveals.length; i++) {
+      let windowHeight = window.innerHeight;
+      let elementTop = reveals[i].getBoundingClientRect().top;
+      let elementVisible = 150;
+
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  window.addEventListener("scroll", initialize);
+  window.addEventListener("load", initialize);
+  return { initialize: initialize };
+})();
+AnimationModule.initialize();
