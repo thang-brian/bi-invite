5adf987a47e895adb9507e96a1d2b5cc1ca1fc37
Thân Trọng Thắng <thangthantrong@gmail.com>
2023-10-11 23:58:55 +0700

    update_v1

--- reveal.js
+++ reveal.js
@@ -1,6 +1,5 @@
 var AnimationModule = (function () {
   function initialize() {
-    reveal();
   }
   function reveal() {
     let reveals = document.querySelectorAll(".reveal");
@@ -16,8 +15,29 @@ var AnimationModule = (function () {
       }
     }
   }
+  function bokeh() {
+    let bokehArea = document.querySelector(".bokeh-area");
+    let bokehNumber = 25;
+    for (let i = 0; i < bokehNumber; i++) {
+      const bokeh = document.createElement("bokeh");
+      bokeh.classList.add("bokeh");
+      const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+      const randomX = Math.random() * 100;
+      const randomY = Math.random() * 100;
+      const randomDuration = Math.random().toFixed(3) * 5 + 5;
+      const randomDelay = Math.random().toFixed(3) * 5;
+      bokeh.style.backgroundColor = "#" + randomColor;
+      bokeh.style.transform = `translate(${randomX}vw, ${randomY}vh)`;
+      bokeh.style.animationDuration = `${randomDuration}s`;
+      bokeh.style.animationDelay = `-${randomDelay}s`;
+      bokehArea.appendChild(bokeh);
+    }
+  }
   window.addEventListener("scroll", initialize);
   window.addEventListener("load", initialize);
+  document.addEventListener("DOMContentLoaded", function () {
+    bokeh();
+  });
   return { initialize: initialize };
 })();
 AnimationModule.initialize();
