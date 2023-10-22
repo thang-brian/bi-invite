7b06d26b761be55db0bece829827759d847f6de3
Thân Trọng Thắng <thangthantrong@gmail.com>
2023-10-22 18:07:11 +0700

    update_v4

--- bi-animation.js
+++ bi-animation.js
@@ -48,20 +48,6 @@ var AnimationModule = (function () {
       confettiShower.push(animated);
     }
   }
-  function reveal() {
-    let reveals = document.querySelectorAll(".reveal");
-    for (let i = 0; i < reveals.length; i++) {
-      let windowHeight = window.innerHeight;
-      let elementTop = reveals[i].getBoundingClientRect().top;
-      let elementVisible = 150;
-
-      if (elementTop < windowHeight - elementVisible) {
-        reveals[i].classList.add("active");
-      } else {
-        reveals[i].classList.remove("active");
-      }
-    }
-  }
   window.addEventListener("scroll", initialize);
   window.addEventListener("load", initialize);
   document.addEventListener("DOMContentLoaded", function () {
